9260e93ac217cc4d9b64759a16f36151c6b3a449
eduairet <hola@eduairet.com>
2023-08-08 22:47:57 -0600

    Server Side Props

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,34 +1,30 @@
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import { getMeetup } from "../utils/services";
+import { getMeetup, getMeetups } from "../utils/services";
 import MeetupDetail from '../components/meetups/MeetupDetail';
 
-
-export default function Meetup() {
-    const id = usePathname(),
-        [meetup, setMeetup] = useState(null),
-        [isLoading, setIsLoading] = useState(true),
-        [error, setError] = useState(null);
-
-    useEffect(() => {
-        if (!id) return;
-        setIsLoading(true);
-        setError(null);
-        getMeetup(id.replace('/', ''))
-            .then(data => {
-                setIsLoading(false);
-                setMeetup(data);
-            }
-            ).catch(
-                err => {
-                    setMeetup(null);
-                    setIsLoading(false);
-                    setError('We couldn\'t get your meetup. Please try again later. :(');
-                }
-            );
-    }, [id]);
-
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>{error}</p>;
+export default function Meetup({ meetup, error }) {
+    if (error) return <p>Error: {error} </p>; {/* Appears if fallback is true */ }
+    if (!meetup && !error) return <p style={{ textAlign: 'center' }}>Could not find meetup</p>;
     return <MeetupDetail meetup={meetup} />;
+}
+
+export async function getStaticPaths(context) {
+    // You can get the params of the URL by using context.params 
+    const meetups = await getMeetups(),
+        meetupsIds = meetups.map(meetup => ({ params: { meetupId: meetup.id } }));
+    return {
+        paths: meetupsIds,
+        fallback: true // It controls the page to render in case the path is not found
+    };
+}
+
+export async function getStaticProps(context) {
+    // You can get the params of the URL by using context.params 
+    const id = context.params.meetupId;
+    let meetup = null, error = null;
+    await getMeetup(id)
+        .then(data => meetup = data)
+        .catch(err => error = err);
+    return {
+        props: { meetup, error }
+    };
 }
