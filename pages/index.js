260ceb195a13ee0146cbfdf162502ccf8eb9e634
eduairet <hola@eduairet.com>
2023-08-08 19:56:07 -0600

    Get Static Props

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,17 @@
-import { useState, useEffect } from "react";
 import { getMeetups } from "../utils/services";
 import MeetupList from "../components/meetups/MeetupList";
 
-export default function Home() {
-    const [meetups, setMeetups] = useState([]),
-        [isLoading, setIsLoading] = useState(true),
-        [error, setError] = useState(null);
+export default function Home({ meetups }) {
+    return <MeetupList meetups={meetups} />;
+}
 
-    useEffect(() => {
-        setIsLoading(true);
-        setError(null);
-        getMeetups()
-            .then(data => {
-                setMeetups(data);
-                setIsLoading(false);
-            }
-            ).catch(
-                err => {
-                    setError('We couldn\'t get your meetups. Please try again later. :(');
-                    setIsLoading(false);
-                }
-            );
-    }, []);
-
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>{error}</p>;
-
-    return (
-        <MeetupList meetups={meetups} />
-    );
+// Next looks for this function to get static props and pre-render the data
+export async function getStaticProps() {
+    // This code is never executed on the client side
+    const meetups = await getMeetups();
+    return {
+        // This is the data that will be passed to the page component
+        props: { meetups },
+        revalidate: 10 // Seconds needed to update the static props
+    };
 }
